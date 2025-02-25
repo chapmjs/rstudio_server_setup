cdd60d341a96e21faa5cd054b9d7d0ba520d515f
John Chapman <john.chapman@systemsresearch.linkpc.net>
2025-02-25 09:02:16 -0700

    additional installation commands

--- setup_rstudio_server.R
+++ setup_rstudio_server.R
@@ -1,5 +1,20 @@
-## install if needed (do this exactly once):
-## sudo su - -c "R -e \"install.packages('usethis', repos='http://cran.rstudio.com/', dependencies=TRUE)\""
+## install if needed:
+## .libPaths() #which library is the root library? /opt/R/4.4.2/lib/R/library
+## sudo su - -c "R -e \"install.packages('usethis', lib='/opt/R/4.4.2/lib/R/library', repos='http://cran.rstudio.com/', dependencies=TRUE)\""
+## library(usethis) - based on error message install missing packages
+## sudo su - -c "R -e \"install.packages('fs', lib='/opt/R/4.4.2/lib/R/library', repos='http://cran.rstudio.com/', dependencies=TRUE)\""
+## sudo su - -c "R -e \"install.packages('rlang', lib='/opt/R/4.4.2/lib/R/library', repos='http://cran.rstudio.com/', dependencies=TRUE)\""
+## sudo su - -c "R -e \"install.packages('glue', lib='/opt/R/4.4.2/lib/R/library', repos='http://cran.rstudio.com/', dependencies=TRUE)\""
+## sudo su - -c "R -e \"install.packages('lifecycle', lib='/opt/R/4.4.2/lib/R/library', repos='http://cran.rstudio.com/', dependencies=TRUE)\""
+
+# ragg package not found
+# textshaping package not found
+# sudo apt install libharfbuzz-dev libfribidi-dev  
+## sudo su - -c "R -e \"install.packages('purrr', lib='/opt/R/4.4.2/lib/R/library', repos='http://cran.rstudio.com/', dependencies=TRUE)\""
+## sudo su - -c "R -e \"install.packages('vctrs', lib='/opt/R/4.4.2/lib/R/library', repos='http://cran.rstudio.com/', dependencies=TRUE)\""
+## sudo su - -c "R -e \"install.packages('cli', lib='/opt/R/4.4.2/lib/R/library', repos='http://cran.rstudio.com/', dependencies=TRUE)\""
+## sudo su - -c "R -e \"install.packages('magrittr', lib='/opt/R/4.4.2/lib/R/library', repos='http://cran.rstudio.com/', dependencies=TRUE)\""
+## sudo su - -c "R -e \"install.packages('gitcreds', lib='/opt/R/4.4.2/lib/R/library', repos='http://cran.rstudio.com/', dependencies=TRUE)\""
 
 library(usethis)
 use_git_config(user.name = "John Doe", user.email = "john@example.org")
